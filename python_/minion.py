bd47f60d34a4d3c483477ace8b76bbe09af22ccd
talwai <aaditya.21@gmail.com>
2014-12-25 15:31:24 -0500

    topic mux for minion

--- python_/minion.py
+++ python_/minion.py
@@ -4,13 +4,16 @@ import zmq
 MASTER_IP = os.environ.get('MASTER_IP', 'localhost')
 MASTER_PORT = os.environ.get('MASTER_PORT', 4550)
 
+SUBSCRIBE_TOPICS = ['PING', 'CMD']
 
 ctx = zmq.Context()
 socket = ctx.socket(zmq.SUB)
+topic_filters = [ bytes(t, 'utf-8') for t in SUBSCRIBE_TOPICS ]
+for tf in topic_filters:
+    socket.setsockopt(zmq.SUBSCRIBE, tf)
+
 socket.connect('tcp://{0}:{1}'.format(MASTER_IP, MASTER_PORT))
 
-topic_filter = bytes('SPORTS', 'utf-8')
-socket.setsockopt(zmq.SUBSCRIBE, topic_filter)
 
 while True:
     s = socket.recv()
