f9f872eb0b905df5e36a4ed501e40f75a762140c
talwai <aaditya.21@gmail.com>
2014-12-25 15:13:29 -0500

    extrace env vars for MASTER

--- python_/master.py
+++ python_/master.py
@@ -1,6 +1,7 @@
+import os
 import zmq
 
-MASTER_PORT = 4550
+MASTER_PORT = os.environ.get('MASTER_PORT', 4550)
 
 ctx = zmq.Context()
 socket = ctx.socket(zmq.PUB)
@@ -8,6 +9,6 @@ socket = ctx.socket(zmq.PUB)
 socket.bind('tcp://*:{}'.format(MASTER_PORT))
 
 while True:
-    string = 'SPORTS abcd'
+    string = 'SPORTS kakkas'
     #print('Sending {}'.format(string))
     socket.send_string(string)
--- python_/minion.py
+++ python_/minion.py
@@ -1,9 +1,13 @@
+import os
 import zmq
 
-MASTER_PORT = 4550
+MASTER_IP = os.environ.get('MASTER_IP', 'localhost')
+MASTER_PORT = os.environ.get('MASTER_PORT', 4550)
+
+
 ctx = zmq.Context()
 socket = ctx.socket(zmq.SUB)
-socket.connect('tcp://localhost:4550')
+socket.connect('tcp://{0}:{1}'.format(MASTER_IP, MASTER_PORT))
 
 topic_filter = bytes('SPORTS', 'utf-8')
 socket.setsockopt(zmq.SUBSCRIBE, topic_filter)
